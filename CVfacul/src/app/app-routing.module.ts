46c8057d1655c250cff5ece2468bc815b7205afa
Lucas Gabriel <diasborges2@gmail.com>
2022-09-10 22:46:35 -0300

    Mais atualizações

--- CVfacul/src/app/app-routing.module.ts
+++ CVfacul/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './components/blog/blog.component';
+import { ConhecimentoComponent } from './components/conhecimento/conhecimento.component';
+import { ExperienciasComponent } from './components/experiencias/experiencias.component';
+import { FormacaoComponent } from './components/formacao/formacao.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: BlogComponent},
+  {path:'formacao', component: FormacaoComponent },
+  {path:'experiencias', component: ExperienciasComponent },
+  {path:'conhecimento', component: ConhecimentoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- CVfacul/src/app/app.module.ts
+++ CVfacul/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormacaoComponent } from './components/formacao/formacao.component';
+import { ExperienciasComponent } from './components/experiencias/experiencias.component';
+import { ConhecimentoComponent } from './components/conhecimento/conhecimento.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    FormacaoComponent,
+    ExperienciasComponent,
+    ConhecimentoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
